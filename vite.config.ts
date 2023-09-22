a598df6ec89e1160aebc73599e433f5cab3c53f2
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-09-22 16:46:30 +0200

    init: simple vue todo

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    browser: {
+      enabled: true,
+      name: 'firefox',
+      provider: 'playwright'
+    }
+  }
+});
