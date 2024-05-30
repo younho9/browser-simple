90ae0713efdd589a41ac255bff72ec0e7e605fdf
Younho Choo <yo+github@younho9.com>
2024-05-30 22:24:01 +0900

    chore: lint

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],
-  test: {
-    browser: {
-      enabled: true,
-      name: "firefox",
-      provider: "playwright",
-    },
-  },
+	plugins: [vue()],
+	test: {
+		browser: {
+			enabled: true,
+			name: "firefox",
+			provider: "playwright",
+		},
+	},
 });
--- vue/app.spec.ts
+++ vue/app.spec.ts
@@ -1,37 +1,37 @@
-import App from './App.vue'
-import { mount } from '@vue/test-utils'
-import {describe, expect, it} from 'vitest'
+import App from "./App.vue";
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
 
-import type { VueWrapper } from '@vue/test-utils'
+import type { VueWrapper } from "@vue/test-utils";
 
 const addTodo = async (wrapper: VueWrapper, text: string) => {
-  const input = wrapper.find('input')
-  const button = wrapper.find('button')
+  const input = wrapper.find("input");
+  const button = wrapper.find("button");
 
-  await input.setValue(text)
-  await button.trigger('click')
-}
+  await input.setValue(text);
+  await button.trigger("click");
+};
 
-describe('rendering App', () => {
-  it('cann add a todo', async () => {
-    const wrapper = mount(App)
-    await addTodo(wrapper, 'new todo')
+describe("rendering App", () => {
+  it("cann add a todo", async () => {
+    const wrapper = mount(App);
+    await addTodo(wrapper, "new todo");
 
-    const todo = wrapper.find('section')
+    const todo = wrapper.find("section");
 
-    expect(todo.find('h1').text()).toBe('new todo')
-    expect(todo.find('input').element.checked).toBe(false)
-    expect(wrapper.text()).toContain('Not all completed')
-  })
+    expect(todo.find("h1").text()).toBe("new todo");
+    expect(todo.find("input").element.checked).toBe(false);
+    expect(wrapper.text()).toContain("Not all completed");
+  });
 
-  it('can toggle a todo', async () => {
-    const wrapper = mount(App)
-    await addTodo(wrapper, 'new todo')
+  it("can toggle a todo", async () => {
+    const wrapper = mount(App);
+    await addTodo(wrapper, "new todo");
 
-    const todo = wrapper.find('section')
-    await todo.find('input').trigger('click')
+    const todo = wrapper.find("section");
+    await todo.find("input").trigger("click");
 
-    expect(todo.find('input').element.checked).toBe(true)
-    expect(wrapper.text()).toContain('All completed')
-  })
-})
+    expect(todo.find("input").element.checked).toBe(true);
+    expect(wrapper.text()).toContain("All completed");
+  });
+});
